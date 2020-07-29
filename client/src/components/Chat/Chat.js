eb695ec779f0674f303a798fe28d8ce5792e28ad
Chidozie <chidough14@gmail.com>
2020-07-29 22:46:47 +0200

    Chat message handling

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,12 +2,23 @@ import React, {useState, useEffect} from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
+import './Chat.css'
+
 let socket
 
 const Chat = ({location})=> {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
-    const ENDPOINT = 'localhost:5000'
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    const ENDPOINT = 'https://flakchat.herokuapp.com/'
+    //const ENDPOINT = 'localhost:5000'
     
 
     useEffect(() => {
@@ -27,8 +38,42 @@ const Chat = ({location})=> {
         }
     }, [ENDPOINT, location.search])
 
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search)
+        socket.on('message', (message)=> {
+            setMessages(messages => [...messages, message])
+            //console.log(message);
+            if(message.user !== name) {
+                console.log('yes');
+                let audio = new Audio('/sound.mp3');
+                audio.play();
+            }
+        })
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });
+    }, [])
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    //console.log(message, messages);
+
     return (
-        <h1>Chat Component</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+            <TextContainer users={users}/>
+        </div>
     )
 }
 
